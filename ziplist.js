fb3522b301c284121284c790b6df2cf4bf2ad60d
Andrew Salazar <35311333+ASalazar46@users.noreply.github.com>
2019-09-23 10:45:20 -1000

    Added files for E27

---
+++ ziplist.js
@@ -0,0 +1,22 @@
+const test1 = [1, 2, 3];
+const test2 = ['a', 'b', 'c'];
+
+function zipList(list1, list2) {
+  if (list1.length !== list2.length) {
+    console.log('List sizes not the same. Returning...');
+    return 0;
+  }
+  const returnList = [];
+  for (let i = 0; i < list1.length; i++) {
+    returnList.push(list1[i]);
+    returnList.push(list2[i]);
+  }
+  return returnList;
+}
+
+function zipListTheSimpleWay(list1, list2) {
+  return _.flatten(_.zip(list1, list2));
+}
+
+console.log(zipList(test1, test2));
+console.log(zipListTheSimpleWay(test1, test2));
